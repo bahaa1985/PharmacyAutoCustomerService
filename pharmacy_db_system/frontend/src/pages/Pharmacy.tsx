160ec3ada611b783e2ac20dce231d6414cb185ea
bahaa1985 <bahaa_salaheldin@live.com>
2026-04-06 20:36:22 +0200

    create pharmacy page - frontend

--- pharmacy_db_system/frontend/src/pages/Pharmacy.tsx
+++ pharmacy_db_system/frontend/src/pages/Pharmacy.tsx
@@ -1,3 +1,39 @@
+import React, {useState} from 'react'
+import { NewPharmacy} from '../features/pharmacy/NewPharmacy';
+import { PharmaciesList } from '../features/pharmacy/PharmaciesList';
 export const PharmacyPage: React.FC=()=>{
-    
+    const tabs = [
+    { label: "Pharmacies List", value: "list" },
+    { label: "New Pharmacy", value: "new" },
+  ];    
+    const [selectedTab, setSelectedTab] = useState<"list" | "new">("new");
+    return (
+        <div className="space-y-8">
+        <div>
+            <h1 className="text-4xl font-bold text-gray-900">Pharmacies</h1>
+            <p className="text-gray-600 mt-2">Manage your pharmacies.</p>
+            <div className="mt-4">
+            <div className="border-b border-gray-200">
+                <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                {tabs.map((tab) => (
+                    <button
+                    key={tab.value}
+                    onClick={() => setSelectedTab(tab.value as "list" | "new")}
+                    className={`whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm ${
+                        selectedTab === tab.value
+                        ? "border-blue-500 text-blue-600"
+                        : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                    }`}
+                    >
+                    {tab.label}
+                    </button>
+                ))}
+                </nav>
+            </div>
+            </div>
+            {selectedTab === "list" && <PharmaciesList/>}
+            {selectedTab === "new" && <NewPharmacy />}
+        </div>
+        </div>
+    )
 }
